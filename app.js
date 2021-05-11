8b0f75d991884d4d402c57bc06c2d03900f8affc
Yumi <terayumim3113@gmail.com>
2021-05-11 13:10:37 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var { Server } = require('socket.io');
+var fs = require('fs');
+var io = new Server(server);
+app.get("/", function (request, response) {
+  response.write(fs.readFileSync('index.html'));
+  response.end();
+});
+app.get("/reception", function (request, response) {
+  response.write(fs.readFileSync('reception.html'));
+  response.end();
+});
+app.get("/room", function (request, response) {
+  response.write(fs.readFileSync('room.html'));
+  response.end();
+});
+app.get("/accounting", function (request, response) {
+  response.write(fs.readFileSync('accounting.html'));
+  response.end();
+});
+
+const main = require('./data');
+io.on('connection', function (socket) {
+  socket.on('client_to_server', function (data) {
+    switch (data.meth) {
+      case 'add':
+        main.add(data.value);
+        break;
+      default:
+        break;
+    }
+    const list = main.allList();
+    io.emit('server_to_client', { value: list })
+  })
+  socket.on('client_to_server_personal', function (data) {
+    switch (data.meth) {
+      case 'enter':
+        console.log(main.fileOutput());
+        let x = listCheck(main.fileOutput(), 'room1', 'reception');
+        console.log('app44' + x);
+        if (x === 'null') {
+          const id = socket.id;
+          console.log('app34' + id);
+          io.to(id).emit('server_to_client', { value: 'nothing', meth: 'enter' });
+        } else {
+          main.changeStatus(x, 'room');
+        }
+        break;
+        case 'leave':
+          let y = listCheck(main.fileOutput(), 'room1', 'room');
+          if (y === 'null') {
+            const id = socket.id;
+            io.to(id).emit('server_to_client', { value: 'nothing', meth: 'leave' });
+          } else {
+            console.log('app57');
+            main.changeStatus(y, 'accounting');
+          }
+          break;
+      case 'end':
+        let z = listCheck(main.fileOutput(), 'room1', 'accounting');
+        if (z === 'null') {
+          const id = socket.id;
+          io.to(id).emit('server_to_client', { value: 'nothing', meth: 'end' });
+        } else {
+          console.log('app67');
+          main.changeStatus(z, 'end');
+        }
+        break;
+    }
+    const list = main.allList();
+    io.emit('server_to_client', { value: list })
+
+  })
+})
+
+function listCheck(list, room, status) {
+  for (i = 0; i < list.length; i++) {
+    if (list[i].room === room && list[i].status === status) {
+      console.log(i);
+      return i;
+    }
+  }
+  return 'null';
+}
+
+server.listen(8000);
---
+++ data.js
@@ -0,0 +1,88 @@
+class Person {
+  constructor(room) {
+    this.room = room;
+    this.status = 'reception';
+    const date = new Date;
+    this.startTime = date;
+  }
+  /**
+ * ステータス変更
+ * @param newStatus 変更するステータス
+ * @param number 変更する人
+ */
+  /**
+ * ルーム変更
+ * @param newroom 変更する部屋
+ * @param number 変更する人
+ */
+  static changeRoom(number, newroom) {
+    let list = fileOutput();
+    list[number].room = newroom;
+    fileInput(list);
+  }
+
+  /**
+ * ステータス変更
+ * @param newStatus 変更するステータス
+ * @param number 変更する人
+ */
+  static changeStatus(number, newStatus) {
+    let list = fileOutput();
+    list[number].status = newStatus;
+    fileInput(list)
+  }
+
+  /**
+ * allList取得
+ */
+  static allList() {
+    let list = fileOutput();
+    let text = [];
+    for (let i = 0; i < list.length; i++) {
+      text[i] = list[i].room + ',' + list[i].status;
+    }
+    return text;
+
+  }
+}
+/**
+ * 人の追加
+ * @param {*} room 割り当てられた部屋
+ */
+function add(room) {
+  a=fileOutput();
+  console.log(room);
+  a.push(new Person(room));
+  fileInput(a);
+}
+
+const fs = require('fs');
+function fileInput(list) {
+  const a = JSON.stringify(list);
+  fs.writeFileSync('listFile', a);
+}
+
+function fileOutput() {
+  const a = fs.readFileSync('listFile')
+  const b = JSON.parse(a || "null");
+  return b;
+}
+
+function fileInput(list) {
+  const a = JSON.stringify(list || "null");
+  fs.writeFileSync('listFile', a);
+}
+
+
+module.exports.changeStatus=Person.changeStatus;
+module.exports.changeRoom=Person.changeRoom;
+module.exports.allList=Person.allList;
+module.exports.add=add;
+module.exports.fileOutput=fileOutput;
+
+function fileReset(){
+  let a=[new Person('room1')];
+  let b=JSON.stringify(a);
+  fs.writeFileSync('listFile',b);
+}
+fileReset();
