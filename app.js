135e68c2aadf32db6a094b981dab50568c0d78a7
terayumi <83050142+terayumi@users.noreply.github.com>
2021-07-19 19:10:26 +0900

    モニター表示機能できた

--- app.js
+++ app.js
@@ -17,22 +17,24 @@ var usersRouter = require('./routes/users');
 var receptionRouter = require('./routes/reception');
 var roomsRouter = require('./routes/rooms');
 var accountingRouter = require('./routes/accounting');
-var correctRouter=require('./routes/correct');
-var callMonitorRouter=require('./routes/callMonitor');
+var correctRouter = require('./routes/correct');
+var callMonitorRouter = require('./routes/callMonitor');
 // socket.io routes
 let main = require('./data')
-let num=0;
+let num = 0;
 io.on('connection', function (socket) {
   socket.on('request', (data) => {
     switch (data.method) {
       case 'add':
-        num=main.add(data.detail);
-        print(data.detail,num);
+        num = main.add(data.detail);
+        print(data.detail, num);
         resList();
         break;
-      case 'room-call':
-        callList=main.call(data.detail,'部屋');
-        io.emit('callList',{value:callList});
+      case 'call':
+        console.log('appcall');
+        callList = main.call(data.detail);
+        io.emit('callList', { value: callList });
+        io.emit('call', data.detail);
         break;
       case 'enter':
         main.changeStatus(data.detail, '診察室');
@@ -42,10 +44,10 @@ io.on('connection', function (socket) {
         main.changeStatus(data.detail, '会計');
         resList();
         break;
-      case 'acco-call':
+      /*case 'acco-call':
         callList=main.call(data.detail,'会計');
         io.emit('callList',{value:callList})
-        break;
+        break;*/
       case 'end':
         main.changeStatus(data.detail, '終了');
         resList();
@@ -54,32 +56,32 @@ io.on('connection', function (socket) {
         resList();
         break;
       case 'reception':
-        main.changeStatus(data.detail,'受付');
+        main.changeStatus(data.detail, '受付');
         resList();
         break;
       case 'newRoom':
-        main.changeRoom(data.detail.num,data.detail.room);
+        main.changeRoom(data.detail.num, data.detail.room);
         resList();
-        io.emit('correct',{num:data.detail.num,detail:data.detail.room});
+        io.emit('correct', { num: data.detail.num, detail: data.detail.room });
         break;
       case 'newStatus':
-        main.changeStatus(data.detail.num,data.detail.status);
+        main.changeStatus(data.detail.num, data.detail.status);
         resList();
-        io.emit('correct',{num:data.detail.num,detail:data.detail.status})
+        io.emit('correct', { num: data.detail.num, detail: data.detail.status })
         break;
     }
-    
-   
+
+
   })
 })
 
-function print(room,num){
-  io.emit('print',{room:room,num:num});
+function print(room, num) {
+  io.emit('print', { room: room, num: num });
 }
 
 
 //リストの送信
-function resList(){
+function resList() {
   io.emit('resList', { value: main.fileOutput() });
 }
 
@@ -91,8 +93,8 @@ app.use('/users', usersRouter);
 app.use('/reception', receptionRouter);
 app.use('/rooms', roomsRouter);
 app.use('/accounting', accountingRouter);
-app.use('/correct',correctRouter);
-app.use('/monitor',callMonitorRouter);
+app.use('/correct', correctRouter);
+app.use('/monitor', callMonitorRouter);
 
 app.use(express.static('public'));
 // catch 404 and forward to error handler
--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 class Person {
-  constructor(number,room) {
-    this.number=number;
+  constructor(number, room) {
+    this.number = number;
     this.room = room;
     this.status = '受付';
     const date = new Date;
@@ -31,29 +31,40 @@ class Person {
 
 }
 
-let callList=[];
-class called{
-  constructor(number,room){
-    this.number=number;
-    this.room=room;
+let callList = [];
+class called {
+  constructor(number, room) {
+    this.number = number;
+    this.room = room;
   }
 }
-function call(number,method){
-  let a;
-  let room;
-  if(method==='部屋'){
-    a=fileOutput();
-    room=a[number].room;
-  }else{
-    room='会計'
+let kari = false;
+function call(number) {
+  let a = fileOutput();
+  let place;
+  if (a[number].status === '会計') {
+    place = '会計';
+  } else {
+    place = a[number].room;
   }
-  callList.push(new called(number,room));
+  kari=false;
+  for (let i = 0; i < callList.length; i++) {
+    if (callList[i].room == place && callList[i].number == number) {
+      console.log('53');
+      kari = true;
+    }
+  }
+  if (kari) {
+
+  } else {
+    callList.push(new called(number, place));
+  }
   return callList;
 }
- /**
- * allList取得
- */
-  function allList() {
+/**
+* allList取得
+*/
+function allList() {
   let list = fileOutput();
   let text = [];
   //リストの0番を表示しないため
@@ -68,10 +79,10 @@ function call(number,method){
  * 人の追加
  * @param {*} room 割り当てられた部屋
  */
-let numb=1;
+let numb = 1;
 function add(room) {
   a = fileOutput();
-  a.push(new Person(a[0].number+1,room));
+  a.push(new Person(a[0].number + 1, room));
   //numb=numb+1;
   a[0].number++;
   fileInput(a);
@@ -102,11 +113,12 @@ module.exports.changeRoom = Person.changeRoom;
 module.exports.allList = allList;
 module.exports.add = add;
 module.exports.fileOutput = fileOutput;
-module.exports.call=call;
+module.exports.call = call;
 function fileReset() {
-  let a=[];
-  a[0] = new Person(0,0);
+  let a = [];
+  a[0] = new Person(0, 0);
   let b = JSON.stringify(a);
   fs.writeFileSync('listFile', b);
 }
-fileReset();
+const cron=require('node-cron');
+cron.schedule('0 0 8 * * *',fileReset);
